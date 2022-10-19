cd1e8515f1c68296f4c71bbc018383799ae6a2e3
ankitamahanta13 <ankitamahanta1323@gmail.com>
2022-10-19 13:06:27 +0530

    Day 1 variable

---
+++ variable/index.js
@@ -0,0 +1,1 @@
+console.log ("subject    marks");
---
+++ variable/problem2.js
@@ -0,0 +1,6 @@
+let name = "Ankita Mahanta";
+console.log (name);
+let x= "Bhramarabara Mahanta";
+console.log (x);
+let y= "Kshirodini Mahanta";
+console.log (y);
