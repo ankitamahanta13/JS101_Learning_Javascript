d3d0476a7eef694193abe022c8b7d35b83f0ec34
ankitamahanta13 <ankitamahanta1323@gmail.com>
2022-10-19 23:53:17 +0530

    day 2 program

---
+++ conditional_statement/index.js
@@ -0,0 +1,5 @@
+
+let a = "ankita";
+let b =  "mahanta"
+console.log (a + b);
+
---
+++ conditional_statement/problem4.js
@@ -0,0 +1,20 @@
+let database_username = "ankita@gmail.com";
+let database_password = "ankita13";
+
+let input_username = "ankita@gmail.com";
+let input_password = "nkita13";
+
+if (database_username == input_username)
+{
+  if (database_password == input_password)
+  {
+    console.log("login successful");
+  }
+  else
+  {
+    console.log("incorrect password");
+  }
+}
+else{
+  console.log("wrong credential");
+}
